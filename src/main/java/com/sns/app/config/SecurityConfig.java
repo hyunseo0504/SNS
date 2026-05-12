368d1e97c109d47edda21420742dcc567343c456
greyth93-ship-it <greyth93@gmail.com>
2026-05-12 12:36:53 +0900

    config

--- src/main/java/com/sns/app/config/SecurityConfig.java
+++ src/main/java/com/sns/app/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.sns.app.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.sns.app.member.MemberServiceImpl;
 import com.sns.app.security.LoginFailHandler;
@@ -20,4 +24,52 @@ public class SecurityConfig {
 	
 	@Autowired
 	private MemberServiceImpl memberServiceImpl;
+	
+	
+	@Bean
+	WebSecurityCustomizer customizer() {
+		return web ->{
+			web.ignoring()
+				.requestMatchers("/css/**")
+				.requestMatchers("/images/**","/img/**","/js/**","/vender/**")
+				.requestMatchers("/files/**", "/fileDown/**")
+				;
+		};
+	}
+	
+	SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception{
+		security
+			.cors(cors->{cors.disable();})
+			.csrf(csrf->{csrf.disable();})
+			.authorizeHttpRequests(auth->{
+				auth
+					.anyRequest().permitAll()
+					;
+			})
+			.formLogin(form->{
+				form
+					.loginPage("/member/login")
+					.loginProcessingUrl("/member/login")
+					.successHandler(successHandler)
+					.failureHandler(failHandler)
+					;
+			})
+			.rememberMe(remember->{
+				remember
+					.rememberMeParameter("rememberMe")
+					.key("rememberKey")
+					.tokenValiditySeconds(60*60*24)
+					.userDetailsService(memberServiceImpl)
+					.authenticationSuccessHandler(successHandler)
+					.useSecureCookie(true)
+					;
+			})
+			.sessionManagement(session->{
+				session
+					.maximumSessions(1)
+					;
+			})
+			;
+			
+	}
 }
